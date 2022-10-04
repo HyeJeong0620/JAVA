d61cf833779bc8e8eb6be02f6ecafe8225ca5af2
HyeJeong0620 <102217632+HyeJeong0620@users.noreply.github.com>
2022-10-04 21:18:37 +0900

    Add files via upload

---
+++ sec01_packageimport/EX01_PackageImport_2/PackageImport_2.java
@@ -0,0 +1,15 @@
+package sec01_packageimport.EX01_PackageImport_2;
+
+import sec01_packageimport.common.A;
+
+public class PackageImport_2 {
+
+	public static void main(String[] args) {
+		A a = new A();
+		
+		System.out.println(a.m);
+		System.out.println(a.n);
+		a.print();
+	}
+
+}
